63ceffb7255944248bf502b0d49b6c03fb4f3506
Michelle <msbrown@users.noreply.github.com>
2017-05-26 13:14:47 -0400

    first_files

---
+++ maps/leaf-demo.js
@@ -0,0 +1,34 @@
+
+// for later:
+// var mapboxAccessToken = {your access token here};
+
+var map = L.map( 'map', {
+  center: [6.5, -8.5],
+  // minZoom: 2,
+  zoom: 7
+})
+
+//Swapt out later:::::
+L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: ['a', 'b', 'c']
+}).addTo( map )
+
+var myURL = jQuery( 'script[src$="leaf-demo.js"]' ).attr( 'src' ).replace( 'leaf-demo.js', '' )
+
+var myIcon = L.icon({
+  iconUrl: myURL + 'images/pin24.png',
+  iconRetinaUrl: myURL + 'images/pin48.png',
+  iconSize: [29, 24],
+  iconAnchor: [9, 21],
+  popupAnchor: [0, -14]
+})
+
+//adds the pins from the merkers.json file
+for ( var i=0; i < markers.length; ++i )
+{
+ L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
+//  .bindPopup( '<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>' )
+  .bindPopup(  'observer: ' + markers[i].name  )
+  .addTo( map );
+}
